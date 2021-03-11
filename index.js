5c44010a22704c2faa533c9273f02afa95e61f01
Christian Torres Gonzalez <alu0101137902@ull.edu.es>
2021-03-11 17:10:55 +0000

    Primera version de la action de hellow world lista

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    const nombreUsuario = core.getInput('who-to-great');
+    const time = (new Date()).toTimeString();
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+    console.log('Hello ' + nombreUsuario);
+    // console.log(`Hello ${nombreUsuario}!`);
+    core.setOutput("time", time);
+    console.log(`The event payload: ${payload}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
